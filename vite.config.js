fa38a36d305930a4f45988af36f84064ae13c8c5
Pralin Karki <127697821+GArgaMonarc27@users.noreply.github.com>
2025-04-21 13:53:55 +1000

    vite yuupdate

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,15 @@
-export default {
-    root: 'src/',
-    publicDir: '../static/',
-    base: './',
-}
+
+import { defineConfig } from 'vite'
+import react from "@vitejs/plugin-react"
+
+export default defineConfig({
+    plugins: [react()],
+    base: "/Space"
+
+});
+    // root: 'src/',
+    // publicDir: '../static/',
+    // base: './',
+
+
+    
